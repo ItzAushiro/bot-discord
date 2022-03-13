d028f950ca41cd672f0e226245a98237fa53a325
ItzAushiro <101271120+ItzAushiro@users.noreply.github.com>
2022-03-13 19:25:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
 const fs = require("fs");
 const chalk = require("chalk");
 
